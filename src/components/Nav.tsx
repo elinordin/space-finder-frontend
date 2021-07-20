b88e1262de5eeb6482e72e7f352339e6f049d029
elinordin <elin.evelina@live.se>
2021-07-20 14:40:49 +0200

    add react-router-dom and links on nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { User } from '../models/Model';
+import { Link } from 'react-router-dom'
 
-interface NavProps {user: User}
+interface NavProps {user: User | undefined}
 
 export default class Nav extends React.Component<NavProps> {
 
     render() {
         return (
-            <div>
-                Home page
-            </div>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/profile'>Profile</Link>
+                <Link to="/login">{this.props.user? 'Login' : 'Logout'}</Link>
+            </nav>
         )
     }
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { User } from '../models/Model';
 
 interface ProfileProps {user: User}
 
