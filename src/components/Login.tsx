eeae085086bd814ca7e36a2f451931be8d61f479
elinordin <elin.evelina@live.se>
2021-07-20 12:50:34 +0200

    add LoginState including initial values

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,7 +5,22 @@ interface LoginProps {
     authService: AuthService
 }
 
-export default class Login extends React.Component<LoginProps> {
+interface LoginState {
+    userName: string,
+    password: string,
+    loginAttempted: boolean,
+    loginSuccessful: boolean
+}
+
+export default class Login extends React.Component<LoginProps, LoginState> {
+
+    //The initial value of the LoginState
+    state: LoginState = {
+        userName: '',
+        password: '',
+        loginAttempted: false,
+        loginSuccessful: false
+    }
 
     render() {
         return (
