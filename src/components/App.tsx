5b1bb1b78ecd36ddda02ab97ab830d56b17bb799
elinordin <elin.evelina@live.se>
2021-07-20 12:36:36 +0200

    include authService in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { User } from '../models/Model'
+import { AuthService } from '../services/AuthService'
 
 interface AppState{
   user: User | undefined
 }
 
 export default class App extends React.Component<{}, AppState>{
+
+  private authService : AuthService = new AuthService();
 
   render() {
     return (
