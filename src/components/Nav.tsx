6fcb433bb1719ed2b12f1fdb6d667cf45c68a96d
elinordin <elin.evelina@live.se>
2021-07-20 15:20:42 +0200

    add a little styling to nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,15 +4,37 @@ import { Link } from 'react-router-dom'
 
 interface NavProps {user: User | undefined}
 
+const styles = {
+    nav: {
+        backgroundColor: 'black',
+        color: 'white',
+        padding: '30px 8%',
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    left: {
+        width: '200px',
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    link: {
+        textDecoration: 'none',
+        color: 'white',
+    }
+}
+
 export default class Nav extends React.Component<NavProps> {
 
     render() {
         return (
-            <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/profile'>Profile</Link>
-                <Link to="/login">{this.props.user? 'Login' : 'Logout'}</Link>
+            <nav style={styles.nav}>
+                <div style={styles.left}>
+                    <Link to='/' style={styles.link}>Home</Link>
+                    <Link to='/profile' style={styles.link}>Profile</Link>      
+                </div>
+                <Link to='/login' style={styles.link}>{this.props.user? 'Logout' : 'Login'}</Link>
             </nav>
         )
     }
 }
+
