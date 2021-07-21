d680696ad7dbfbaf5fb5799b8c4868623f49f4f7
elinordin <elin.evelina@live.se>
2021-07-21 16:39:33 +0200

    add styles to popup

--- src/components/ReservationPopup.tsx
+++ src/components/ReservationPopup.tsx
@@ -1,5 +1,5 @@
-
 import React from 'react';
+import './ReservationPopup.css'
 
 interface ReservationPopupProps {
     visible: boolean,
@@ -8,15 +8,18 @@ interface ReservationPopupProps {
     close: () => void
 }
 
+
 export default class ReservationPopup extends React.Component <ReservationPopupProps> {
     
     render() {
         if (this.props.visible) {
             return (
-                <div>
-                    <h2>{this.props.available? 'Reservation successful' : 'Reservation failed'}</h2>
-                    <p>{this.props.available? `Your booking ID is: ${this.props.id}` : 'Please try another day'}</p>
-                    <button onClick={this.props.close}>Ok, got it!</button>
+                <div className='background'>
+                    <div className='popup'>
+                        <h2>{this.props.available? 'Reservation successful' : 'Reservation failed'}</h2>
+                        <p>{this.props.available? `Your booking ID is: ${this.props.id}` : 'Please try another day'}</p>
+                        <button onClick={this.props.close}>Ok, got it!</button>
+                    </div>
                 </div>
             )
         } else {return null}
