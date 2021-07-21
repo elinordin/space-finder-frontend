de1a882c8e723e637dc894bb4bf60caabd52daea
elinordin <elin.evelina@live.se>
2021-07-21 14:47:56 +0200

    add content and props to Space component

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
-export default class Space extends React.Component<{},{}>{
-    render(){
+interface SpaceProps {
+    id: string,
+    name: string,
+    location: string,
+    photo: string,
+    reserve: (id: string) => void
+}
+
+export default class Space extends React.Component<SpaceProps>{
+    render() {
         return (
             <div>
-                Space
+                <img src={this.props.photo? this.props.photo : 'https://picsum.photos/200/300'} alt=''/>
+                <ul>
+                    <li>{this.props.id}</li>
+                    <li>{this.props.name}</li>
+                    <li>{this.props.location}</li>
+                </ul>
+                <button onClick={() => this.props.reserve(this.props.id)}>Reserve</button>
             </div>
+
         )
     }
 }
