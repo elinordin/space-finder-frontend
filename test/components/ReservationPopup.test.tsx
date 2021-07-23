574f9718369634d99f26bc1d7078c3d975424247
elinordin <elin.evelina@live.se>
2021-07-23 12:19:20 +0200

    add all tests to popup

--- test/components/ReservationPopup.test.tsx
+++ test/components/ReservationPopup.test.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom'
+import { fireEvent } from '@testing-library/react'
 
 import ReservationPopup from '../../src/components/ReservationPopup'
 
@@ -7,7 +8,7 @@ describe('Popup test suite', () => {
 
     let container : HTMLDivElement
 
-    const closeMock = jest.fn
+    const closeMock = jest.fn()
 
     beforeAll(() => {
         container = document.createElement('div')
@@ -24,11 +25,65 @@ describe('Popup test suite', () => {
         beforeEach(() => {
             ReactDOM.render(<ReservationPopup visible={true} available={true} id='1' close={closeMock}/>, container)
         })
+
+
+        test('Renders correct HTML-elements', () => {
+            const background = document.querySelector('.background')
+            const popup = document.querySelector('.popup')
+            const heading = document.querySelector('h2')
+            const bookingText = document.querySelector('p')
+            const button = document.querySelector('.close-btn')
+
+            expect(background).toBeInTheDocument()
+            expect(popup).toBeInTheDocument()
+            expect(heading).toBeInTheDocument()
+            expect(bookingText).toBeInTheDocument()
+            expect(button).toBeInTheDocument()
+        })
+
+
+        test('Renders the correct content', () => {
+            const heading = document.querySelector('h2')
+            const bookingText = document.querySelector('p')
+
+            expect(heading).toHaveTextContent('Reservation successful')
+            expect(bookingText).toHaveTextContent('Your booking ID is: 1')
+        })
+
+
+        test('Calls the close function on click of button', () => {
+            const closeButton = document.querySelector('.close-btn')!
+            fireEvent.click(closeButton)
+
+            expect(closeMock).toHaveBeenCalled()
+        })
     })
+
 
     describe('With unavailable reservation', () => {
         beforeEach(() => {
             ReactDOM.render(<ReservationPopup visible={true} available={false} id='2' close={closeMock}/>, container)
+        })
+
+        test('Renders the correct content', () => {
+            const heading = document.querySelector('h2')
+            const bookingText = document.querySelector('p')
+
+            expect(heading).toHaveTextContent('Reservation failed')
+            expect(bookingText).toHaveTextContent('Please try another day')
+        })
+        
+    })
+
+
+    describe('When it is not visible', () => {
+        beforeEach(() => {
+            ReactDOM.render(<ReservationPopup visible={false} available={false} id='2' close={closeMock}/>, container)
+        })
+
+        test('Renders no content', () => {
+            const popup = document.querySelector('.popup')
+            expect(popup).not.toBeInTheDocument()
         })
         
     })
