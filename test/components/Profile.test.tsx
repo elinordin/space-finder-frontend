436c63873ca0ba3adc1b112729286a7312c21395
elinordin <elin.evelina@live.se>
2021-07-23 12:43:19 +0200

    add additional setup to Profile test

--- test/components/Profile.test.tsx
+++ test/components/Profile.test.tsx
@@ -1,10 +1,29 @@
 import ReactDOM from 'react-dom'
+import { StaticRouter } from 'react-router'
 
 import Profile from '../../src/components/Profile'
 import { User } from '../../src/models/Model'
 
 
 const someUser : User = {name: 'userName', email: 'some@email.com'}
+const userAttributes = [
+    {
+        key: 'Description', 
+        value: 'Yo yo yo! I am the best user'
+    },
+    {
+        key: 'Age', 
+        value: 'Soon to be 27'
+    },    
+    {
+        key: 'Country', 
+        value: 'Sweden'
+    },    
+    {
+        key: 'Job', 
+        value: 'Engineer'
+    }
+]
 
 
 describe('Profile test suite', () => {
@@ -12,24 +31,25 @@ describe('Profile test suite', () => {
     let container : HTMLDivElement
     
     const authServiceMock = {getUserAttributes: jest.fn()}
-    
-
 
     beforeAll(() => {
+        authServiceMock.getUserAttributes.mockResolvedValue(userAttributes)
         container = document.createElement('div')
         document.body.appendChild(container)
-        
     })
 
     afterAll(() => {
         document.removeChild(container)
         container.remove()
+        jest.clearAllMocks()
     })
 
 
     describe('With a logged in user', () => {
 
-        beforeEach(() => ReactDOM.render(<Profile user={someUser} authService={authServiceMock as any}/>, container))
+        beforeEach(() => ReactDOM.render(
+            <Profile user={someUser} authService={authServiceMock as any}/>
+        , container))
 
         test('Test 1', () => {
 
@@ -39,7 +59,9 @@ describe('Profile test suite', () => {
 
     describe('Without a logged in user', () => {
 
-        beforeEach(() => ReactDOM.render(<Profile user={undefined} authService={authServiceMock as any}/>, container))
+        beforeEach(() => ReactDOM.render(
+            <StaticRouter><Profile user={undefined} authService={authServiceMock as any}/></StaticRouter>
+        , container))
 
         test('Test 1', () => {
 
